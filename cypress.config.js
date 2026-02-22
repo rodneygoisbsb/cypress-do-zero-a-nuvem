414fa78e46252cd329fb37a1041f3dd5699bf8c0
Rodney Góis <rodneygoisofc@gmail.com>
2026-02-22 00:13:32 -0300

    Configure cypress cloud 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "9psjxu"
+  projectId: "9psjxu",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
